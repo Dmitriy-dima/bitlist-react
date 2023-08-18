0d3a8ab8031773b54a232ae882b69fcf14514fe9
Dmitriy Dima <133665751+nevermor-kkuzehn@users.noreply.github.com>
2023-08-18 11:42:36 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,8 +10,8 @@ export const App = () => {
 	return (
 		<div className='app'>
 			<Header />
-			<Home />
 			<div className="container">
+				<Home />
 				<Sidebar />
 				<AppRoutes />
 			</div>
